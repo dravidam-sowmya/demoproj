1b53187799f4c38b2f1c404c2e2970d2bad09a40
dravidam-sowmya <dravidamsowmya23@gmail.com>
2020-12-21 19:54:14 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DragDropModule} from '@angular/cdk/drag-drop';
+import { MatDialogModule,} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ColumnFilterComponent } from './column-filter/column-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    ColumnFilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ColumnFilterComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA]
+  
 })
 export class AppModule { }
