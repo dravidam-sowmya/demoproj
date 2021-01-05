76205715843a7bb4c0bcec5a30aa4fe3bddf2bd1
dravidam-sowmya <dravidamsowmya23@gmail.com>
2021-01-05 18:45:29 +0530

    virtusa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import{CdkDragDrop,moveItemInArray,transferArrayItem} from '@angular/cdk/drag-drop';
 import {MatDialog} from '@angular/material/dialog';
 // import { DialogData } from './DialogData';
 import {ColumnFilterComponent} from '../app/column-filter/column-filter.component'
 import { of } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 interface employee {
   [key: string]: string;
 }
@@ -17,8 +18,21 @@ interface emp {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public dialog: MatDialog) {}
-
+  cargoCapacity: any;
+  manufacturer: any;
+  consumables: any;
+  maxAtmospheringSpeed: any;
+  created: any;
+  constructor(public dialog: MatDialog,private http: HttpClient) {}
+listOfDetails:any;
+ResultModel:any;
+  ngOnInit(){
+    this.http.get('https://swapi.dev/api/starships/').subscribe(result=>{
+       this.listOfDetails =result['results']
+     
+    })
+   
+  }
   title = 'POC';
   employeeColumnDetails:employee={
     Name : 'employeeName',
@@ -55,7 +69,19 @@ employeeDataList:employee[] = [
     });
     this.listOfColumnsToDisplay = dialogRef.afterClosed();
     
-   
   }
+  OnSelectionChange(event){
+    let targetValue=event.target.value
+    let trial=this.listOfDetails.map(item=>{
+      if(item.name==targetValue){
+        this.ResultModel=item.model
+        this.cargoCapacity=item['cargo_capacity']
+        this.manufacturer=item['manufacturer']
+        this.consumables=item['consumables']
+        this.maxAtmospheringSpeed=item['max_atmosphering_speed']
+        this.created=item['created']
+      }
+    })
 
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { DragDropModule} from '@angular/cdk/drag-drop';
 import { MatDialogModule,} from '@angular/material/dialog';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ColumnFilterComponent } from './column-filter/column-filter.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import { ColumnFilterComponent } from './column-filter/column-filter.component';
     AppRoutingModule,
     DragDropModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
